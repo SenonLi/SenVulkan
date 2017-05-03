8e675f400e029945cfb9d6ccfc72d8534a97a330
SenonLi <senonli6510@gmail.com>
2017-05-02 22:28:13 -0700

    Start Texture tutorial

--- vsSenVulkan/main.cpp
+++ vsSenVulkan/main.cpp
@@ -10,44 +10,44 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#include "Support/SenAbstractGLFW.h"
-#include <functional>
-
-
-int main() {
-	SenAbstractGLFW widget;
-
-	try {
-		widget.showWidget();
-	}
-	catch (const std::runtime_error& e) {
-		std::cerr << e.what() << std::endl;
-
-#ifdef _DEBUG
-		OutputDebugString("\n\n");
-		OutputDebugString(e.what());
-		MessageBox(NULL, e.what(), "Vulkan Error!", 0);
-#endif
-
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS;
-}
-
-
-//#include "SenVulkanTutorial/Tutorial_Triangle.h"
+//#include "Support/SenAbstractGLFW.h"
+//#include <functional>
+//
 //
 //int main() {
-//	Tutorial_Triangle widget;
+//	SenAbstractGLFW widget;
 //
 //	try {
-//		widget.run();
+//		widget.showWidget();
 //	}
 //	catch (const std::runtime_error& e) {
 //		std::cerr << e.what() << std::endl;
+//
+//#ifdef _DEBUG
+//		OutputDebugString("\n\n");
+//		OutputDebugString(e.what());
+//		MessageBox(NULL, e.what(), "Vulkan Error!", 0);
+//#endif
+//
 //		return EXIT_FAILURE;
 //	}
 //
 //	return EXIT_SUCCESS;
 //}
+
+
+#include "SenVulkanTutorial/Tutorial_Triangle.h"
+
+int main() {
+	Tutorial_Triangle widget;
+
+	try {
+		widget.run();
+	}
+	catch (const std::runtime_error& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
